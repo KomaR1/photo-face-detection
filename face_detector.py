89d2996dc37195bb9c328bcd5f71bb44913cfb71
sypachov <sypachov@gmail.com>
2020-06-27 23:30:18 +0300

    Fixing bugs

--- face_detector.py
+++ face_detector.py
@@ -7,12 +7,12 @@ img = cv2.imread("photo.jpg")
 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=5)
+faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=7)
 
 for x, y, w, h in faces:
     img = cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
-resized = cv2.resize(img, (int(img.shape[1]/3), int(img.shape[0]/3)))
+resized = cv2.resize(img, (int(img.shape[1]), int(img.shape[1])))
 
 cv2.imshow("Gray", resized)
 cv2.waitKey(0)
